826b91d4ceea17398732b731b0cedad0f3671654
blackawa <3104kurokawa@gmail.com>
2018-08-29 13:58:42 +0900

    :bug: define params variable

--- src/navi/handler/command.clj
+++ src/navi/handler/command.clj
@@ -6,7 +6,8 @@
 (defmethod ig/init-key ::assign [_ {:keys [db logger]}]
   (fn [{{channel-id :channel_id
          team-id :team_id
-         text :text} :params :as req}]
+         text :text
+         :as params} :params :as req}]
     (log logger :info :receive-assign-command params)
     ;; TODO: 今までのアサイン履歴から次の担当者を決める
     ;; TODO: 新しいアサイン履歴を保存する
